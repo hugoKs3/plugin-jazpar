be357cd2ff14ff8be5ec4dc3bdf4ce266a94f13f
hugoKs3 <hugo@questroy.com>
2021-01-01 15:02:13 +0100

    fix display

--- core/class/jazpar.class.php
+++ core/class/jazpar.class.php
@@ -730,6 +730,7 @@ $postfields = "javax.faces.partial.ajax=true&javax.faces.source=_eConsoconsoDeta
                 $cmdHistory = history::byCmdIdDatetime($cmdMonth->getId(), $dateCompare);
                 if (is_object($cmdHistory)) {
                     $value = round($cmdHistory->getValue(), 0);
+                    $value = 2000;
                     $cmd = $this->getCmd(null, 'localmin');
                     if (is_object($cmd)) {
                         $min = round($cmd->execCmd(), 0);
