c3222a4c1c5ac0af6b805be18a9f762c3ddf925d
hugoKs3 <hugo@questroy.com>
2022-01-02 15:24:50 +0100

    fix login issue

--- core/class/jazpar.class.php
+++ core/class/jazpar.class.php
@@ -270,7 +270,13 @@ class jazpar extends eqLogic {
         log::add(__CLASS__, 'error', $this->getHumanName() . ' Authentification error');
         return null;
       } else {
-        log::add(__CLASS__, 'info', $this->getHumanName() . ' ...authenticated!');
+        $obj = json_decode($response);
+        if ($obj->state !== "SUCCESS") {
+          log::add(__CLASS__, 'error', $this->getHumanName() . ' Authentification error');
+          return null;
+        } else {
+          log::add(__CLASS__, 'info', $this->getHumanName() . ' ...authenticated!');
+        }
       }
 
       log::add(__CLASS__, 'info', $this->getHumanName() . ' Retrieve PCE...');
