e4d9f6b914fbfe3a8745220cbd539e096668d67b
hugoKs3 <hugo@questroy.com>
2022-02-01 20:55:23 +0100

    days delay option

--- core/class/jazpar.class.php
+++ core/class/jazpar.class.php
@@ -457,6 +457,8 @@ class jazpar extends eqLogic {
     public static function preConfig_daysdelay($value) {
       if (filter_var($value, FILTER_VALIDATE_INT, $options) === false) {
         throw new Exception(__('Le nombre de jours doit être un entier positif ou 0',__FILE__));
+      } else {
+        config::save('daysdelay', $value, 'jazpar');
       }
     }
 
